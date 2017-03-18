760fe128f69df18606bd00f9c7d95fa4fd42e322
Benjamin Ißler <b.issler@mwaysolutions.com>
2017-03-18 02:20:34 +0100

    move login button after password field

---
+++ background.js
@@ -0,0 +1,10 @@
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (changeInfo.status == 'complete' && tab.active) {
+
+    chrome.tabs.executeScript(null, {file: 'script.js'});
+
+    console.log("executed");
+
+  }
+});
+
--- popup.js
+++ popup.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var checkPageButton = document.getElementById('checkPage');
-    checkPageButton.addEventListener('click', function () {
+  var checkPageButton = document.getElementById('checkPage');
+  checkPageButton.addEventListener('click', function () {
+
+    chrome.tabs.executeScript(null, {file: 'script.js'});
 
 
-        chrome.tabs.executeScript( null, {file: 'script.js'});
 
-    }, false);
+  }, false);
 }, false);
