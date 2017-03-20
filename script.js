1add5f1ffc2b1a096d0a1be8b9da904a9834531f
Benjamin Ißler <b.issler@mwaysolutions.com>
2017-03-21 00:27:12 +0100

    add gitignore, clean up script.js

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
-//document.querySelector("body").style.background = "black";
-//document.querySelector(".module-page.session.login").style.background = "transparent";
-
 // delete safed usernames
 localStorage.removeItem("localforage/mw_localUsers");
 
 // stay logged in
-document.querySelector("input[type=checkbox][name=rememberMe]").checked = true
+document.querySelector("input[type=checkbox][name=rememberMe]").checked = true;
 
 // move login button after the password field
 function insertAfter(newNode, referenceNode) {
     referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
 insertAfter(document.querySelector('#btn-login'), document.querySelector('.input-password'));
-
-// stay logged in
-// document.getElementById('#btn-login').disabled = false;
