d081c57db83cc73a9f63883fc5cfc73dc19b5a3e
Benjamin Ißler <b.issler@mwaysolutions.com>
2017-03-25 21:09:04 +0100

    fix disabled login button

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+now=$(date +"%Y%m%d_%H%M")
+
+zip -r relutionEnhancer_${now}.zip background.js icon128.png icon16.png icon48.png manifest.json script.js
--- script.js
+++ script.js
@@ -1,13 +1,33 @@
-// delete safed usernames
-localStorage.removeItem("localforage/mw_localUsers");
-
-// stay logged in
-document.querySelector("input[type=checkbox][name=rememberMe]").checked = true;
-
-// move login button after the password field
 function insertAfter(newNode, referenceNode) {
-    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+  referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
-insertAfter(document.querySelector('#btn-login'), document.querySelector('.input-password'));
 
-console.info("Relution Enhancer applied");
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function runExtension() {
+  await sleep(500);
+
+  var rememberMeCheckbox = document.querySelector('.remember-me');
+  var loginButton = document.querySelector('#btn-login');
+  var passwordField = document.querySelector('.input-password');
+
+  // delete safed usernames
+  localStorage.removeItem("localforage/mw_localUsers");
+
+  // remove "Remember Me" checkbox
+  if (rememberMeCheckbox !== null) {
+    document.querySelector('.remember-me').remove();
+  }
+
+  // move login button after the password field
+  if (loginButton !== null && passwordField !== null) {
+    loginButton.disabled = false;
+    insertAfter(loginButton, passwordField);
+  }
+
+  console.info("Relution Enhancer applied");
+}
+
+runExtension();
