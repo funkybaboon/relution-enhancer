0c67c4b347fa413c98130f2bf526b35a4a41d4d4
Benjamin Ißler <b.issler@mwaysolutions.com>
2017-03-27 16:21:41 +0200

    only execute changes on the correct site

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+rm *.zip
+
 now=$(date +"%Y%m%d_%H%M")
 
 zip -r relutionEnhancer_${now}.zip background.js icon128.png icon16.png icon48.png manifest.json script.js
--- script.js
+++ script.js
@@ -9,25 +9,45 @@ function sleep(ms) {
 async function runExtension() {
   await sleep(500);
 
-  var rememberMeCheckbox = document.querySelector('.remember-me');
-  var loginButton = document.querySelector('#btn-login');
-  var passwordField = document.querySelector('.input-password');
 
-  // delete safed usernames
-  localStorage.removeItem("localforage/mw_localUsers");
+    function escapeRegExp(str) {
+      return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+    }
 
-  // remove "Remember Me" checkbox
-  if (rememberMeCheckbox !== null) {
-    document.querySelector('.remember-me').remove();
+    function matchUrl() {
+      var currentUrl = window.location.toString();
+      var loginUrl =
+        escapeRegExp("https://") +
+        '[^\\.]+' +
+        escapeRegExp(".relution.io/relution/portal/#/login");
+      loginUrl = new RegExp(loginUrl);
+
+      return result = loginUrl.test(currentUrl);
+    }
+
+    if (matchUrl()) {
+
+    var rememberMeCheckbox = document.querySelector('.remember-me');
+    var loginButton = document.querySelector('#btn-login');
+    var passwordField = document.querySelector('.input-password');
+
+    // delete safed usernames
+    localStorage.removeItem("localforage/mw_localUsers");
+
+    // remove "Remember Me" checkbox
+    if (rememberMeCheckbox !== null) {
+      document.querySelector('.remember-me').remove();
+    }
+
+    // move login button after the password field
+    if (loginButton !== null && passwordField !== null) {
+      loginButton.disabled = false;
+      insertAfter(loginButton, passwordField);
+    }
+
+    console.info("Relution Enhancer applied");
   }
 
-  // move login button after the password field
-  if (loginButton !== null && passwordField !== null) {
-    loginButton.disabled = false;
-    insertAfter(loginButton, passwordField);
-  }
-
-  console.info("Relution Enhancer applied");
 }
 
 runExtension();
